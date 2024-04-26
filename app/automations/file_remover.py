4409f4605bb5c2179e0e3d68e9b98a2dccc13956
macy <gft.chyb@extaccount.com>
2024-04-26 07:29:27 +0200

    test commit for checking repo

--- app/automations/file_remover.py
+++ app/automations/file_remover.py
@@ -3,7 +3,7 @@ import os
 dir = "C:/Users/macy/Downloads/"
 ext = (".ica")
 
-def remove_files():
+def remove_files() -> None:
     for files in os.listdir(dir):
         if files.endswith(ext):
             print(files)
