c2e301f1c390921ccf2dc00d5805e8a54e61738a
macy <gft.chyb@extaccount.com>
2024-05-06 14:39:48 +0200

    added requests counter

--- app/app.py
+++ app/app.py
@@ -4,6 +4,9 @@ from project.config import get_config
 import os
 from project.health.routes import health
 
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
+
 app = Flask(__name__)
 app.register_blueprint(health)
 
@@ -20,10 +23,16 @@ cert_file = os.path.join(current_dir, 'cert.pem')
 key_file = os.path.join(current_dir, 'key.pem')
 ssl_context = (cert_file, key_file)
 
+REQUESTS = Counter('http_requests_total', 'Total HTTP Requests', ['method', 'endpoint'])
 
 @app.route('/')
 def test():
-    return '<h1>Hello from Flask & Docker</h2>'
+    REQUESTS.labels(method='GET', endpoint='/').inc()
+    return 'Hello from Flask & Docker'
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 @app.route('/add_data', methods=['POST'])
 def add_data(): 
